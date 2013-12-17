adf44d4142ae850bf69ed083b2ab6e884531b049
Hugo Quijada <hquijada@edesarrollos.com>
2013-12-17 16:59:50 -0700

    Actualización tabla usuario

--- data/schema/usuario.sql
+++ data/schema/usuario.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `posicion` tinyint(4) NOT NULL,
+  `nombre` varchar(200) NOT NULL
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE  `usuario` ADD  `posicion` TINYINT NOT NULL ;
